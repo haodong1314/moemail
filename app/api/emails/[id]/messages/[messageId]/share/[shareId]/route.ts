6b6a70a4257869743e53d0ca913e1e011b295262
haodong1314 <1628564641@qq.com>
2026-04-25 12:55:02 +0800

    Update route.ts

--- app/api/emails/[id]/messages/[messageId]/share/[shareId]/route.ts
+++ app/api/emails/[id]/messages/[messageId]/share/[shareId]/route.ts
@@ -4,7 +4,7 @@ import { eq, and } from "drizzle-orm"
 import { NextResponse } from "next/server"
 import { getUserId } from "@/lib/apiKey"
 
-export const runtime = "edge"
+
 
 // 删除消息分享链接
 export async function DELETE(
